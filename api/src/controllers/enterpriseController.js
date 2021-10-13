33f8c48b5ba99be60aa7810019030ea0bd03cd8d
Nico Constantin <nicoconstantin11@gmail.com>
2021-10-13 17:13:24 -0300

    CRUD ready and tested with postman

--- api/src/controllers/enterpriseController.js
+++ api/src/controllers/enterpriseController.js
@@ -1,19 +1,56 @@
 const Enterprise = require('../db/models/enterpriseModel.js');
 
 function createEnterprise (req,res,next){ 
-    
+
+    let { name, address, NIT, phone} = req.body;
+
+    const enterpriseInstance = new Enterprise ({
+        name,
+        address,
+        NIT,
+        phone
+    });
+
+    enterpriseInstance.save()
+    .then(()=>res.send('Enterprise created'))
+    .catch(e=>next(e))
 };
 
 function readEnterprise (req,res,next){ 
-    
+    Enterprise.find()
+    .then(rta=>res.send(rta))
+    .catch(e=>next(e))
 };
 
 function updateEnterprise (req,res,next){ 
-    
+    let { id, name, address, NIT, phone } = req.body;
+
+    Enterprise.findById( id )
+    .then(enterpriseFound=>{
+        if(name){
+            enterpriseFound.name = name
+        }
+        if(address){
+            enterpriseFound.address = address
+        }
+        if(NIT){
+            enterpriseFound.NIT = NIT
+        }
+        if(phone){
+            enterpriseFound.phone = phone
+        }
+        return enterpriseFound.save()
+    })
+    .then(enterpriseUpdated=>res.send(enterpriseUpdated))
+    .catch(e=>next(e))
 };
 
 function deleteEnterprise (req,res,next){ 
+    let { id } = req.body;
 
+    Enterprise.deleteOne({ _id : id })
+    .then((rta)=>res.send(rta))
+    .catch(e=>next(e))
 };
 
 
--- api/src/db/models/enterpriseModel.js
+++ api/src/db/models/enterpriseModel.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose')
 
 const enterpriseSchema = new mongoose.Schema({
+
   name: {
     type: String,
     required: true,
     lowercase:true,
   },
+
   address: {
     type: String,
     required: true,
     lowercase:true,
   },
+
   NIT:{
     type: String,
     required: true,
@@ -19,10 +22,16 @@ const enterpriseSchema = new mongoose.Schema({
       message: 'No es un valor NIT válido'
     }
   },
-  Phone:{
+
+  phone:{
     type: Number,
     required: true,
+    validate:{
+      validator: (v) => v.toString().length === 10,
+      message: 'El número debe tener 10 caracteres para ser válido'
+    }
   },
+
   });
 
 const Enterprise = mongoose.model('Enterprise', enterpriseSchema);
