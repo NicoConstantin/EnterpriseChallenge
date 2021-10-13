844c36a7fb3e6a0e662c2445cddcdb8756352236
Nico Constantin <nicoconstantin11@gmail.com>
2021-10-13 15:35:45 -0300

    model created

--- api/src/db/models/enterpriseModel.js
+++ api/src/db/models/enterpriseModel.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose')
 
 const enterpriseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    lowercase:true,
+  },
+  address: {
+    type: String,
+    required: true,
+    lowercase:true,
+  },
+  NIT:{
+    type: String,
+    required: true,
+    validate:{
+      validator: (v) => /(^[0-9]+-{1}[0-9]{1})/.test(v),
+      message: 'No es un valor NIT válido'
+    }
+  },
+  Phone:{
+    type: Number,
+    required: true,
+  },
   });
 
 const Enterprise = mongoose.model('Enterprise', enterpriseSchema);
